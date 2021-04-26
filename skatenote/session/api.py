93b0fac3e91bcbb2b6d4b9ced6b4fc1e58cf5f0f
SeanZheng21 <SeanZheng21@gmail.com>
2021-04-27 02:13:51 +0800

    Build session query

--- skatenote/session/api.py
+++ skatenote/session/api.py
@@ -4,14 +4,20 @@ from .serializers import SessionSerializer
 
 # Session viewset
 class SessionViewSet(viewsets.ModelViewSet):
-    queryset = Session.objects.all()
+    # queryset = Session.objects.all()
     # permission_classes = [permissions.IsAuthenticated]
     permission_classes = [permissions.AllowAny]
     serializer_class = SessionSerializer
 
-    # def get_queryset(self):
-    #     # print(dir(self.request.user.practice_set.all()))
-    #     return self.request.user.practice_set.all()
+    def get_queryset(self):
+        # print(dir(Session.objects.all()))
+
+        all_practice = self.request.user.practice_set.all()
+        all_practice_ids = [elt.id for elt in all_practice]
+        # print(all_practice_ids)
+        result = Session.objects.filter(practice__in=all_practice_ids)
+        print(result)
+        return result
 
     # def perform_create(self, serializer):
     #     serializer.save(user=self.request.user)
